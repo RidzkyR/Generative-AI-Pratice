5cd427e1d373b884280d98617506d9f02534274d
ridzky rahim <ridzkyrhm37@gmail.com>
2024-04-22 19:38:05 +0700

    complete helper

--- Latihan Generative AI (BERT QA) - start/app/src/main/java/com/dicoding/bertqa/BertQAHelper.kt
+++ Latihan Generative AI (BERT QA) - start/app/src/main/java/com/dicoding/bertqa/BertQAHelper.kt
@@ -6,18 +6,28 @@ import android.util.Log
 import org.tensorflow.lite.gpu.CompatibilityList
 import org.tensorflow.lite.task.core.BaseOptions
 import org.tensorflow.lite.task.text.qa.BertQuestionAnswerer
+import org.tensorflow.lite.task.text.qa.QaAnswer
 
 //inisialisasi model ML
-class BertQAHelper0(val context: Context){
+class BertQAHelper0(
+    val context: Context,
+    private val resultAnswerListener: ResultAnswerListener?,
+) {
     private var bertQuestionAnswerer: BertQuestionAnswerer? = null
+
+    //melepas memori
+    fun clearBertQuestionAnswerer() {
+        bertQuestionAnswerer?.close()
+        bertQuestionAnswerer = null
+    }
 
     private fun setupBertQuestionAnswerer() {
 
         val baseOptionsBuilder = BaseOptions.builder()
 
-        if (CompatibilityList().isDelegateSupportedOnThisDevice){
+        if (CompatibilityList().isDelegateSupportedOnThisDevice) {
             baseOptionsBuilder.useGpu()
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1){
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
             baseOptionsBuilder.useNnapi()
         } else {
             // Menggunakan CPU
@@ -32,9 +42,36 @@ class BertQAHelper0(val context: Context){
             bertQuestionAnswerer =
                 BertQuestionAnswerer.createFromFileAndOptions(context, BERT_QA_MODEL, options)
         } catch (e: IllegalStateException) {
+            resultAnswerListener
+                ?.onError("Bert Question Answerer gagal untuk terinisialisasi")
             Log.e(TAG, "TFLite gagal untuk load model dengan error: " + e.message)
         }
 
+    }
+
+    //
+    fun getQuestionAnswer(topicsContent: String, question: String) {
+        if (bertQuestionAnswerer == null) {
+            setupBertQuestionAnswerer()
+        }
+
+        // Inference time: selisih waktu saat sebelum dan setelah mengeksekusi proses inferensi
+        var inferenceTime = System.currentTimeMillis()
+
+        val answers = bertQuestionAnswerer?.answer(topicsContent, question)
+
+        inferenceTime = System.currentTimeMillis() - inferenceTime
+
+        resultAnswerListener?.onResults(answers, inferenceTime)
+    }
+
+    // kemungkinan jawaban nanti
+    interface ResultAnswerListener {
+        fun onError(error: String)
+        fun onResults(
+            results: List<QaAnswer>?,
+            inferenceTime: Long
+        )
     }
 
     companion object {
